383e176e250c6e0b85f2de2a9cd4bff7f259b5ea
Bramg <bramgoudsmid@hotmail.com>
2020-09-08 23:29:04 +0200

    Merge remote-tracking branch 'origin/master' into master

--- src/H04/Ellips.java
+++ src/H04/Ellips.java
@@ -1,17 +1,14 @@
-package H03;
+package H04;
 
 import java.awt.*;
-import java.applet.*;
+import java.applet.Applet;
 
-public class Show extends Applet {
-
+public class Ellips extends Applet {
     public void init() {
     }
-
     public void paint(Graphics g) {
         setBackground(Color.blue);
         g.setColor(Color.yellow);
-        g.drawRect(20, 20, 100, 50);
-        g.fillRect(20, 80, 100, 50);
+        g.fillArc(20, 20, 150, 200, 90, 360);
     }
 }
--- src/H04/Show.java
+++ src/H04/Show.java
@@ -1,14 +1,17 @@
-package H03;
+package H04;
 
 import java.awt.*;
-import java.applet.Applet;
+import java.applet.*;
 
-public class Ellips extends Applet {
+public class Show extends Applet {
+
     public void init() {
     }
+
     public void paint(Graphics g) {
         setBackground(Color.blue);
         g.setColor(Color.yellow);
-        g.fillArc(20, 20, 150, 200, 90, 360);
+        g.drawRect(20, 20, 100, 50);
+        g.fillRect(20, 80, 100, 50);
     }
 }
