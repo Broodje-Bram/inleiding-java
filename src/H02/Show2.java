2770ece4d7556c295c0e2e72b667c2ffccbe3b98
Bramg <bramgoudsmid@hotmail.com>
2020-09-08 20:38:29 +0200

    Tekst veranderd

---
+++ src/H02/Show2.java
@@ -0,0 +1,14 @@
+package H02;
+
+import java.awt.*;
+import java.applet.Applet;
+
+public class Show2 extends Applet {
+    public void paint(Graphics g) {
+        g.setColor(Color.blue);
+        g.drawString("Bram", 50, 60 );
+        g.setColor(Color.red);
+        g.drawString("Goudsmid", 50, 80 );
+    }
+
+}
