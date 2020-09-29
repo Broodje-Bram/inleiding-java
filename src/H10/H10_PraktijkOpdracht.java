b57b25d635605d5e29286ff2d999998500819bf4
Bramg <bramgoudsmid@hotmail.com>
2020-09-29 16:00:27 +0200

    1e Versie

--- src/H10/H10_PraktijkOpdracht.java
+++ src/H10/H10_PraktijkOpdracht.java
@@ -119,18 +119,19 @@ public class H10_PraktijkOpdracht extends Applet {
             g.drawLine(230,300,280,280);
         }
         else if(getal>7){
+            drawSmiley(Color.green, g);
             // Oval for face outline
-            g.setColor(Color.YELLOW);
-            g.fillOval(180, 170, 150, 150);
+            //g.setColor(Color.green);
+            //g.fillOval(180, 170, 150, 150);
 
             // Ovals for eyes
             // with black color filled
-            g.setColor(Color.BLACK);
-            g.fillOval(220, 220, 15, 15);
-            g.fillOval(270, 220, 15, 15);
+            //g.setColor(Color.BLACK);
+            //g.fillOval(220, 220, 15, 15);
+            //g.fillOval(270, 220, 15, 15);
 
             // Arc for the smile
-            g.drawArc(230, 280, 50, 20, 180, 180);
+            //g.drawArc(230, 280, 50, 20, 180, 180);
         }
 
         g.setFont(new Font ("Default", Font.BOLD, 15 ));
@@ -138,4 +139,19 @@ public class H10_PraktijkOpdracht extends Applet {
         g.setColor(Color.red);
         g.drawString("" + getal + beoordeling, 20, 80);
     }
+    private void drawSmiley (Color color, Graphics g){
+        g.setColor(color);
+        g.fillOval(180, 170, 150, 150);
+
+        // Ovals for eyes
+        // with black color filled
+        g.setColor(Color.BLACK);
+        g.fillOval(220, 220, 15, 15);
+        g.fillOval(270, 220, 15, 15);
+
+        // Arc for the smile
+        g.drawArc(230, 280, 50, 20, 180, 180);
+    }
+
+
 }
--- src/H14/H14_01.java
+++ src/H14/H14_01.java
@@ -1,4 +1,41 @@
 package H14;
 
-public class H14_01 {
+import java.awt.*;
+import java.applet.Applet;
+import java.awt.event.*;
+
+public class H14_01 extends Applet {
+    double rand1;
+    double rand2;
+
+    String kleur[] = {"Harten", "Schoppen", "Klaver", "Ruiten"};
+    String betekenis[] = {"Aas", "Twee", "Drie", "Vier", "Vijf", "Zes", "Zeven", "Acht", "Negen", "Tien", "Boer", "Vrouw", "Heer"};
+    String gekozen = "";
+    Button knop;
+
+    public void init() {
+        knop = new Button("Druk op de knop om een kaart te kiezen");
+        knop.addActionListener(new knopListener());
+        add(knop);
+    }
+
+    public void paint(Graphics g) {
+        g.setFont(new Font ("Default", Font.BOLD, 15 ));
+        setBackground(Color.CYAN);
+        g.drawString(gekozen,50,50);
+    }
+
+
+    private class knopListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            rand1 = Math.random() * 13;
+            int r1 = (int) rand1;
+            rand2 = Math.random() * 4;
+            int r2 = (int) rand2;
+
+            gekozen = kleur[r2] + "" + betekenis[r1];
+
+            repaint();
+        }
+    }
 }
